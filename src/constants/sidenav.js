0d2f3397d7edd6f2e7465377bfdaa1a96bc39728
MunozCesarCM <munozcesar.cm@gmail.com>
2023-04-18 13:48:09 -0600

    Header icons & Sidenav

---
+++ src/constants/sidenav.js
@@ -0,0 +1,40 @@
+export const navItems = [
+  {
+    id: 300,
+    title: 'Digital Content & Devices',
+    children: [
+      { id: 400, title: 'Amazon Music' },
+      { id: 401, title: 'Kindle E-readers & Books' },
+      { id: 402, title: 'Amazon Appstore' },
+    ],
+  },
+  {
+    id: 301,
+    title: 'Shop By Department',
+    children: [
+      { id: 403, title: 'Electronics' },
+      { id: 404, title: 'Computers' },
+      { id: 405, title: 'Smart Home' },
+      { id: 406, title: 'Arts & Crafts' },
+    ],
+  },
+  {
+    id: 302,
+    title: 'Programs & Features',
+    children: [
+      { id: 403, title: 'Gift Cards' },
+      { id: 404, title: 'Shop By Interest' },
+      { id: 405, title: 'Amazon Live' },
+      { id: 406, title: 'International Shopping' },
+    ],
+  },
+  {
+    id: 303,
+    title: 'Help & Settings',
+    children: [
+      { id: 407, title: 'Your Account' },
+      { id: 408, title: 'Customer Service' },
+      { id: 409, title: 'Sign In' },
+    ],
+  },
+];
