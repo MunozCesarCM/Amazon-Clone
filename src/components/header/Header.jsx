f9c38bdf6a47619526e7fb4e06d4f49662d9ba45
MunozCesarCM <munozcesar.cm@gmail.com>
2023-04-20 17:56:40 -0600

    Completed header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import Bottom from './Bottom';
+import { categories } from '../../constants/header';
+import { logo } from '../../assets/index';
+
+const Header = () => {
+  const [showAll, setShowAll] = useState(false);
+
+  return (
+    <header>
+      <div className='w-full bg-amazon_blue text-white px-4 py-3 pl-6 flex items-center gap-4'>
+        <div className='header-hover'>
+          <img className='w-14' src={ logo } alt="logo" />
+        </div>
+        <div className='header-hover items-center gap-1'>
+          <PlaceOutlinedIcon />
+          <p className='text-sm text-light_text font-light flex flex-col'>Deliver to
+            <span className='text-sm text-white_text font-semibold -mt-1'> Home</span>
+          </p>
+        </div>
+        <div className='h-10 rounded-md flex flex-grow relative'>
+          <span
+            onClick={ () => setShowAll(!showAll) }
+            className='search-container w-14 bg-gray-200 hover:bg-gray-300 border-2 duration-300 text-sm text-amazon_blue font-title_font rounded-tl-md rounded-bl-md'>
+              All <span></span><ArrowDropDownIcon />
+          </span>
+          {
+            showAll && (
+              <div>
+                <ul className='absolute w-56 h-80 top-10 left-0 overflow-y-scroll overflow-x-hidden bg-white border-[1px] border-amazon_blue text-black p-2 flex-col gap-1 z-2'>
+                {
+                  categories.map((item) => (
+                    <li key={item.id} className='text-sm tracking-wide font-title_font border-b-[1px] border-b-transparent hover:border-b-amazon_blue cursor-pointer duration-200'>{ item.title }</li>
+                  ))
+                }
+                </ul>
+              </div>
+            )
+          }
+          <input className='h-full text-base text-amazon_blue flex-grow outline-none border-none px-2' type='text' />
+          <span className='search-container w-12 bg-amazon_yellow hover:bg-[#F3A847] text-amazon_blue rounded-tr-md rounded-br-md'>
+            <SearchIcon />
+          </span>
+        </div>
+        <div className='flex flex-col items-start justify-center header-hover'>
+          <p className='text-xs text-light_text font-light'>Hello, sign in</p>
+          <p className='text-sm font-semibold -mt-1 text-white_text'>
+            Accounts & Lists <span><ArrowDropDownIcon /></span>
+          </p>
+        </div>
+        <div className='flex flex-col items-start justify-center header-hover'>
+          <p className='text-xs text-light_text font-light'>Returns</p>
+          <p className='text-sm font-semibold -mt-1 text-white_text'>
+            & Orders
+          </p>
+        </div>
+        <div className='flex items-start justify-center relative header-hover'>
+          <ShoppingCartOutlinedIcon />
+          <p className='text-xs font-semibold mt-3 text-white'>
+            Cart <span className='absolute text-xs -top-1 left-6 font-semibold p-1 h-4 bg-[#F3A847] text-amazon_blue rounded-full flex justify-center items-center'>0</span>
+          </p>
+        </div>
+      </div>
+      <Bottom />
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/header/SideNav.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useRef } from 'react';
+import { motion } from 'framer-motion';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import CloseIcon from '@mui/icons-material/Close';
+import { navItems } from '../../constants/sidenav';
+
+const SideNav = ({ setSideNav }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener('click', (event) => {
+      if (event.target.contains(ref.current)) {
+        setSideNav(false);
+      }
+    })
+  },[]);
+
+  return (
+    <div className='w-full h-screen text-black fixed top-0 left-0 bg-amazon_blue bg-opacity-80'>
+      <div className='w-full h-full relative'>
+        <motion.div
+          ref={ref}
+          initial={{x:-500, opacity:0}}
+          animate={{x:0, opacity:1}}
+          transition={{duration:.3}}
+          className='w-[350px] h-full bg-white'>
+          <div className='w-full bg-amazon_light text-white py-2 px-6 flex items-center gap-4'>
+            <AccountCircleIcon />
+            <h3 className='font-title_font font-bold text-lg tracking-wide'>
+              Hello, sign in
+            </h3>
+          </div>
+          {navItems.map((item) => (
+            <div key={ item.id } className='py-3 border-t-[1px] border-b-gray-300'>
+              <h3 className='text-lg font-title_font font-semibold mb-1 px-6'>
+                { item.title }
+              </h3>
+              <ul>
+                {item.children.map((child) => (
+                  <li key={ child.id } className='flex text-sm items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer'>
+                    { child.title }<span><KeyboardArrowRightIcon /></span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+          <span className='cursor-pointer absolute top-0 left-[360px] w-10 h-10 text-white flex items-center justify-center hover:text-zinc-200 duration-300'>
+            <CloseIcon onClick={ () => setSideNav(false) } />
+          </span>
+        </motion.div>
+      </div>
+    </div>
+  )
+}
+
+export default SideNav;
