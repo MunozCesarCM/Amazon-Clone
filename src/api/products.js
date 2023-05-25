6470356cf00b08af4c08c7dc5d0ce66137f4a23d
MunozCesarCM <munozcesar.cm@gmail.com>
2023-05-25 16:23:17 -0600

    Basic caching

--- src/api/products.js
+++ src/api/products.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
+let productsCache = null;
+
 export async function fetchProducts() {
-  const products = await axios.get(
+  if (productsCache !== null) return productsCache;
+
+  productsCache = await axios.get(
     'https://fakestoreapi.com/products'
   );
-  return products;
+  return productsCache;
 }
